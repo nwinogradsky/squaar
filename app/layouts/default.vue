30fe207deb1fdf9399afde24062f5ca688286418
nwinogradsky <ton_email_github@example.com>
2025-10-31 19:31:04 +0100

    First !

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,20 +1,14 @@
 <template>
   <v-app>
-    <v-img src="~/public/background.png" cover :height="mdAndUp ? '100vh' : undefined">
+    <v-img src="./background.png" cover height="100vh">
       <v-sheet
         class="fill-height text-center align-content-center pa-2"
-        color="rgba(var(--v-theme-primary), 0.7)"
+        color="transparent"
       >
-        <v-container :class="xs ? 'px-2 py-10 mb-10' : undefined">
+        <v-container>
           <slot />
         </v-container>
       </v-sheet>
     </v-img>
-    <!--      <Footer />-->
   </v-app>
 </template>
-<script setup lang="ts">
-import { useDisplay } from 'vuetify'
-// import Footer from '../components/Footer.vue'
-const { xs, mdAndUp } = useDisplay()
-</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,13 @@ import vuetify from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-    css: ['vuetify/styles', '@mdi/font/css/materialdesignicons.css'],
+    css: ['vuetify/styles'],
     build: {
         transpile: ['vuetify'],
     },
     vite: {
         ssr: {
-            noExternal: ['vuetify'], // important pour le rendu SSR
+            noExternal: ['vuetify'],
         },
         plugins: [
             vuetify({
