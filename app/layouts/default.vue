f2fd37ce6068fac082b672e96bacc9eb244e3bb9
nwinogradsky <nwinogradsky@gmail.com>
2025-10-31 19:06:30 +0100

    First !

---
+++ app/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-img src="~/public/background.png" cover :height="mdAndUp ? '100vh' : undefined">
+      <v-sheet
+        class="fill-height text-center align-content-center pa-2"
+        color="rgba(var(--v-theme-primary), 0.7)"
+      >
+        <v-container :class="xs ? 'px-2 py-10 mb-10' : undefined">
+          <slot />
+        </v-container>
+      </v-sheet>
+    </v-img>
+    <!--      <Footer />-->
+  </v-app>
+</template>
+<script setup lang="ts">
+import { useDisplay } from 'vuetify'
+// import Footer from '../components/Footer.vue'
+const { xs, mdAndUp } = useDisplay()
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+Page
+</template>
+<script setup lang="ts">
+</script>
